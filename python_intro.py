a62f67daf63e6fea10c5d12eb227e5c0b46753ca
Mustapha Tarawally <Mustapha Tarawally>
2016-04-23 15:15:23 +0100

    My Django Girls all, first commit

---
+++ python_intro.py
@@ -0,0 +1,15 @@
+print("hello world")
+if 3>5:
+ print("yay")
+else: print("aww")
+def hi():
+hi()
+     print("hi there")
+     print("how are ya")
+     def hi(name):
+         if name == "madiha":
+             print(" hia Madiha")
+         elif name == "kadi":
+             print(" hi kadi")
+         else:
+             print ( "heyoo")
